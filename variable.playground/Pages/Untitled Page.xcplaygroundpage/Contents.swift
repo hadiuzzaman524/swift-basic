656dd92dc8026b80f2beca8488d5b91c8fee21aa
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2023-09-23 15:53:55 +0600

    chore: learn about variable

---
+++ variable.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+// Variable
+/*
+ 
+ There are several data types
+ 1. Int
+ 2. Double
+ 3. Float
+ 4. String
+ 5. Char
+ 6. Touple
+ */
+//Int
+var data: Int
+data = 200
+print(data)
+
+//Double
+var doubleData : Double = 3.22
+print(doubleData)
+
+//Float
+var floatData : Float
+floatData = 2.23
+print(floatData)
+
+//String
+var stringData : String
+stringData = "I Love Swift"
+print(stringData)
+
+//Character
+var charData : Character
+charData = "A"
+print(charData)
+
+//Printing variable
+var name = "Hadiuzzaman"
+var age = 25
+print("Name: \(name) Age: \(age)")
